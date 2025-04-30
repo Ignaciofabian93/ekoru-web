542628c64e0c0753687779a16bd88e08051d16d2
Ignaciofabian93 <ignaciofabian93@gmail.com>
2025-04-29 21:01:09 -0400

    mods

--- config/environment.ts
+++ config/environment.ts
@@ -1,3 +1,5 @@
-export const ENVIRONMENT = process.env.ENVIRONMENT ?? "development";
-export const GRAPHQL_URL = ENVIRONMENT !== "development" ? "http://74.208.96.137:4000" : "http://localhost:4000";
-export const REST_URL = ENVIRONMENT !== "development" ? "http://74.208.96.137:4000/auth" : "http://localhost:4000/auth";
+export const ENVIRONMENT: "development" | "production" = "production";
+export const GRAPHQL_URL = "http://74.208.96.137:4000";
+// export const GRAPHQL_URL = "http://localhost:4000";
+export const REST_URL = "http://74.208.96.137:4000/auth";
+// export const REST_URL = "http://localhost:4000/auth";
--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  env: {
-    ENVIRONMENT: process.env.ENVIRONMENT,
-  },
   /* config options here */
 };
 
