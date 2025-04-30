2a0a9fd296d5b5c640053aec8b14c0592be3acf4
Ignaciofabian93 <ignaciofabian93@gmail.com>
2025-04-29 20:26:40 -0400

    next conf

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   env: {
     ENVIRONMENT: process.env.ENVIRONMENT,
+  },
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname),
+    };
+    return config;
   },
   /* config options here */
 };
