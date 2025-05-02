9461e1f77b447b4d795c4c3f8c72973b26a9d593
Ignaciofabian93 <ignaciofabian93@gmail.com>
2025-05-02 11:58:52 -0400

    urls

--- config/environment.ts
+++ config/environment.ts
@@ -1,3 +1,3 @@
 export const ENVIRONMENT = "production";
-export const GRAPHQL_URL = "http://gateway:4000";
-export const REST_URL = "http://gateway:4000/auth";
+export const GRAPHQL_URL = "http://69.48.204.85:4000";
+export const REST_URL = "http://69.48.204.85:4000/auth";
