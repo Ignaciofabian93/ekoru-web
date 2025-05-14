f391b7e0bfe353529f2008a74e1884308a3b5799
Ignaciofabian93 <ignaciofabian93@gmail.com>
2025-05-14 16:04:45 -0400

    url

--- config/environment.ts
+++ config/environment.ts
@@ -1,3 +1,3 @@
 export const ENVIRONMENT = process.env.ENVIRONMENT || "development";
-export const GRAPHQL_URL = ENVIRONMENT !== "development" ? "https://gateway.ekoru.cl" : "http://localhost:4000";
-export const REST_URL = ENVIRONMENT !== "development" ? "https://gateway.ekoru.cl/auth" : "http://localhost:4000/auth";
+export const GRAPHQL_URL = ENVIRONMENT !== "development" ? "http://gateway:4000" : "http://localhost:4000";
+export const REST_URL = ENVIRONMENT !== "development" ? "http://gateway:4000/auth" : "http://localhost:4000/auth";
