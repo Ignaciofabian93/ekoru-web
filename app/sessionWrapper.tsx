c55452fbd7fa5be79bac8cf1d4c50925eed1edea
Ignaciofabian93 <ignaciofabian93@gmail.com>
2025-05-17 14:15:57 -0400

    loading spinner

--- app/sessionWrapper.tsx
+++ app/sessionWrapper.tsx
@@ -1,35 +1,37 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLazyQuery } from "@apollo/client";
 import { GET_PROFILE } from "@/graphql/auth/query";
 import { toast } from "react-toastify";
-import { useRouter, usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 import useSessionStore from "@/store/session";
 
 export default function SessionWrapper({ children, token }: { children: React.ReactNode; token: string | undefined }) {
-  console.log("SESSION TOKEN::", token);
-
+  const [loading, setLoading] = useState<boolean>(true);
   const router = useRouter();
-  const pathname = usePathname();
   const { handleSession, setIsAuthenticated, data } = useSessionStore();
   const notifyError = (error: string) => toast.error(error);
 
-  const [GetMe, { error: authError }] = useLazyQuery(GET_PROFILE);
+  const [GetMe, { error: authError, loading: authLoading }] = useLazyQuery(GET_PROFILE);
 
   const handleUserData = async () => {
+    setLoading(true);
     const { data: userData } = await GetMe();
     if (authError) {
       notifyError("Ha ocurrido un error con los datos de sesión");
       router.replace("/auth");
+      setLoading(false);
       return;
     }
     if (userData) {
       handleSession(userData.me);
       setIsAuthenticated(true);
+      setLoading(false);
     }
   };
 
   useEffect(() => {
     if (!token) {
+      setLoading(false);
       router.replace("/auth");
     } else {
       if (!data.name) {
@@ -37,6 +39,13 @@ export default function SessionWrapper({ children, token }: { children: React.Re
       }
     }
   }, [token, data]);
+
+  if (loading || authLoading)
+    return (
+      <div className="flex items-center justify-center h-screen bg-white">
+        <div className="w-16 h-16 border-4 border-primary border-t-transparent border-solid rounded-full animate-spin"></div>
+      </div>
+    );
 
   return <>{children}</>;
 }
--- app/wrapper.tsx
+++ app/wrapper.tsx
@@ -3,7 +3,5 @@ import { cookies } from "next/headers";
 
 export default async function AppWrapper({ children }: { children: React.ReactNode }) {
   const token = (await cookies()).get("token")?.value;
-  console.log("WRAPPER TOKEN::", token);
-
   return <Providers token={token}>{children}</Providers>;
 }
