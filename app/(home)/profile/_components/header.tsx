c8c44c8a993f7519215fa552ef27aaa2154dc9a9
Ignaciofabian93 <ignaciofabian93@gmail.com>
2025-05-28 16:40:56 -0400

    image animation

--- app/(home)/profile/_components/header.tsx
+++ app/(home)/profile/_components/header.tsx
@@ -1,5 +1,7 @@
 "use client";
+import { useState } from "react";
 import { Pencil } from "lucide-react";
+import { motion, AnimatePresence } from "framer-motion";
 import Modal from "@/components/modal/modal";
 import useSessionStore from "@/store/session";
 import clsx from "clsx";
@@ -8,12 +10,42 @@ import ProfileForm from "./form";
 
 export default function ProfileHeader() {
   const { data, edit, toggleEdit } = useSessionStore();
+  const [expandImage, setExpandImage] = useState<boolean>(false);
   const image = data?.profileImage || "/brandIcon.webp";
+
+  const toggleExpandImage = () => setExpandImage(!expandImage);
+
+  const ExpandedImageView = () => {
+    return (
+      <AnimatePresence>
+        <motion.div
+          className="fixed inset-0 z-[99999] flex items-center justify-center bg-black/40 backdrop-blur-sm"
+          onClick={toggleExpandImage}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <motion.div
+            className="w-[260px] h-[260px] md:w-[320px] md:h-[320px] lg:w-[450px] lg:h-[450px] rounded-full overflow-hidden relative shadow-xl border-4 border-white"
+            initial={{ scale: 0.5, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            exit={{ scale: 0.5, opacity: 0 }}
+            transition={{ type: "spring", stiffness: 300, damping: 25 }}
+          >
+            <Image src={image} alt="perfil" className="object-cover" fill priority />
+          </motion.div>
+        </motion.div>
+      </AnimatePresence>
+    );
+  };
 
   return (
     <div className={clsx("relative w-full flex items-center justify-start", "px-8 py-2 mt-4 mb-8")}>
-      <div className={clsx("w-[90px] h-[90px] rounded-full overflow-hidden", "mr-8", "bg-white")}>
-        <Image src={image} alt="perfil" className="w-full h-full object-cover" width={90} height={90} />
+      <div
+        className={clsx("w-[90px] h-[90px] rounded-full overflow-hidden", "mr-8", "bg-white", "cursor-pointer")}
+        onClick={toggleExpandImage}
+      >
+        <Image src={image} alt="perfil" className="w-full h-full object-cover" width={90} height={90} priority />
       </div>
       <div className="flex flex-col items-start justify-center">
         <span className="text-[20px] font-semibold">
@@ -32,6 +64,7 @@ export default function ProfileHeader() {
           <ProfileForm />
         </Modal>
       )}
+      <AnimatePresence>{expandImage && <ExpandedImageView />}</AnimatePresence>
     </div>
   );
 }
