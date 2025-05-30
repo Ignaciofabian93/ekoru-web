1869804a169398a95dc8b109647bc3f375bb7b3a
Ignaciofabian93 <ignaciofabian93@gmail.com>
2025-05-30 11:50:08 -0400

    changes

--- app/(home)/feed/_components/hero.tsx
+++ app/(home)/feed/_components/hero.tsx
@@ -1,12 +1,15 @@
 "use client";
 import { useEffect, useState } from "react";
 import Carousel from "@/components/carousel/carousel";
-import image1 from "@/assets/images/img1.jpg";
-import image2 from "@/assets/images/img2.jpg";
-import image3 from "@/assets/images/img3.jpg";
+import image1 from "@/assets/images/exchange.jpg";
+import image2 from "@/assets/images/market.jpg";
+import image3 from "@/assets/images/stores.jpg";
+import image4 from "@/assets/images/benefits.jpg";
+import image5 from "@/assets/images/challenges.jpg";
+import image6 from "@/assets/images/community.jpg";
 import clsx from "clsx";
 
-const images = [image1, image2, image3];
+const images = [image1, image2, image3, image4, image5, image6];
 
 export default function Hero() {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
--- app/(home)/feed/page.tsx
+++ app/(home)/feed/page.tsx
@@ -1,20 +1,25 @@
+"use client";
 import PageWrapper from "../components/pageWrapper";
 import Stories from "../components/stories";
 import Hero from "./_components/hero";
 import Banner from "../../../components/banner/banner";
-import InfoSection from "./_components/infoSection";
 import PageSection from "@/components/section/pageSection";
+import dynamic from "next/dynamic";
+
+const DynamicInfoSection = dynamic(() => import("./_components/infoSection"), {
+  ssr: false,
+});
 
 export default function FeedPage() {
   return (
     <PageWrapper>
-      <Stories />
+      {/* <Stories /> */}
       <Hero />
       <Banner
         title="¿hasta dónde llega tu impacto?"
         description="Pequeños retos, grandes cambios. ¡Empieza hoy a descubrirlos!"
       />
-      <InfoSection />
+      <DynamicInfoSection />
       <Banner
         title="dale una segunda vida"
         description="¡Tu elección hace la diferencia! Reduce, reutiliza, impacta."
