13600fd106d9f07aa10126c7094b00e8b0ac377f
Ignaciofabian93 <ignaciofabian93@gmail.com>
2025-06-29 16:03:32 -0400

    logout update

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -1,13 +1,13 @@
 import { NextResponse } from "next/server";
 
 export async function POST() {
-  return NextResponse.json(
+  const response = NextResponse.json(
     { message: "Sesión cerrada" },
     {
       status: 200,
-      headers: {
-        "Set-Cookie": "token=; Path=/; Max-Age=0; HttpOnly; Secure; SameSite=Strict",
-      },
     }
   );
+  response.headers.append("Set-Cookie", "token=; Path=/; Max-Age=0; HttpOnly; Secure; SameSite=Strict");
+  response.headers.append("Set-Cookie", "refreshToken=; Path=/; Max-Age=0; HttpOnly; Secure; SameSite=Strict");
+  return response;
 }
