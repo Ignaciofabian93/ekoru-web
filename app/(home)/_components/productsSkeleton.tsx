633bbfa52b39c95093b57bf027d44f2e9a587b39
Ignaciofabian93 <ignaciofabian93@gmail.com>
2025-07-20 17:56:23 -0400

    feed refresh fixed

--- app/(home)/_components/productsSkeleton.tsx
+++ app/(home)/_components/productsSkeleton.tsx
@@ -2,7 +2,10 @@ export default function ProductsSkeleton() {
   return (
     <div className="w-full flex overflow-x-auto gap-x-4 pb-4 scrollbar-thin scrollbar-thumb-green-200">
       {Array.from({ length: 6 }).map((_, index) => (
-        <div key={index} className="min-w-[180px] max-w-[180px] h-[350px] bg-gray-200 animate-pulse rounded-2xl" />
+        <div
+          key={index}
+          className="min-w-[164px] max-w-[164px] h-[350px] bg-gray-200 animate-pulse rounded-2xl"
+        />
       ))}
     </div>
   );
--- app/(home)/feed/_hooks/useExchangeableProducts.tsx
+++ app/(home)/feed/_hooks/useExchangeableProducts.tsx
@@ -1,32 +1,37 @@
 import { useLazyQuery } from "@apollo/client";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { GET_FEED_PRODUCTS } from "../_graphql/products";
 import { Product } from "@/types/product";
+import { usePathname } from "next/navigation";
 import useAlert from "@/hooks/useAlert";
 
 type Scope = "MARKET" | "STORE";
 
 export default function useExchangeableProducts({ scope, exchange }: { scope: Scope; exchange: boolean }) {
+  const pathname = usePathname();
   const { notifyError } = useAlert();
-  const [products, setProducts] = useState<Product[]>([]);
-
-  const [Products, { loading }] = useLazyQuery(GET_FEED_PRODUCTS);
+  const [fetchProducts, { data, loading, error, refetch }] = useLazyQuery(GET_FEED_PRODUCTS, {
+    fetchPolicy: "network-only",
+  });
 
   useEffect(() => {
-    async function fetchProducts() {
-      const { data } = await Products({ variables: { take: 10, scope, exchange } });
-      if (data) {
-        setProducts(data.feedProducts);
-      } else {
-        notifyError("No se encontraron productos");
-      }
-    }
+    fetchProducts({ variables: { take: 10, scope, exchange } });
+  }, [fetchProducts, scope, exchange]);
 
-    fetchProducts();
-  }, []);
+  useEffect(() => {
+    if (error) {
+      notifyError("No se encontraron productos");
+    }
+  }, [error]);
+
+  // If the pathname changes, refetch products
+  useEffect(() => {
+    refetch({ take: 10, scope, exchange });
+  }, [pathname, refetch, scope, exchange]);
 
   return {
-    products,
+    products: (data?.feedProducts as Product[]) || [],
     loading,
+    refetch: () => refetch({ take: 10, scope, exchange }),
   };
 }
