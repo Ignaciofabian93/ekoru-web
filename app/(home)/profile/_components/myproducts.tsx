5de46048a55d113ba22fc0c18a0b63de11891440
Ignaciofabian93 <ignaciofabian93@gmail.com>
2025-07-20 20:32:24 -0400

    updt

--- app/(home)/profile/_components/myproducts.tsx
+++ app/(home)/profile/_components/myproducts.tsx
@@ -8,7 +8,9 @@ export default function MyProducts() {
   const { myProducts, data } = useProfile();
 
   return (
-    <div className={clsx("w-full min-h-[400px] flex flex-col items-center justify-start", "mx-auto px-4 mb-8")}>
+    <div
+      className={clsx("w-full min-h-[400px] flex flex-col items-center justify-start", "mx-auto px-4 mb-8")}
+    >
       <SectionTitle title="Mis Productos" />
       {myProducts.length > 0 ? (
         <div className="w-full flex overflow-x-scroll no-scrollbar items-center justify-start gap-4 mt-4 px-1 py-4">
@@ -19,10 +21,10 @@ export default function MyProducts() {
               title={product.name}
               images={product.images}
               price={product.price}
-              seller={data.name.split(" ")[0]}
-              sellerImage={data.profileImage}
+              seller={data?.name?.split(" ")[0]}
+              sellerImage={data?.profileImage}
               description={product.description}
-              location={`${data.county.county}, ${data.city.city}`}
+              location={`${data?.county?.county}, ${data?.city?.city}`}
             />
           ))}
         </div>
